060c2576b2c63ecbd66920e2721af03a5aab415d
sangeetha <sangeetharenganath98@gmail.com>
2025-10-27 23:47:12 +0530

    update for cors

--- src/infrastructure/entry-points/api/app.ts
+++ src/infrastructure/entry-points/api/app.ts
@@ -20,7 +20,8 @@ app.use(cors({
       'http://localhost:3000',
       'http://localhost:5174',
       'http://127.0.0.1:5173',
-      'http://127.0.0.1:3000'
+      'http://127.0.0.1:3000',
+      'https://health-platform-frontend.vercel.app'
     ];
 
     if (allowedOrigins.includes(origin)) {
